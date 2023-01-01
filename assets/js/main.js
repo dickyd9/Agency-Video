2d0c38e16f2c9b559c92f20b517ffef40a1b32a6
dicky <dickyd6@gmail.com>
2023-01-01 13:10:04 +0700

    modal play video

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,25 @@
 var myNav = document.getElementById("nav");
+let mybutton = document.getElementById("myBtn");
+let loader = document.getElementById("preloader");
 
 window.onscroll = function () {
-    "use strict";
-    if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280) {
-        myNav.classList.add("scroll");
+  "use strict";
+  if (window.matchMedia("only screen and (max-width: 768px)")) {
+    if (
+      document.body.scrollTop >= 60 ||
+      document.documentElement.scrollTop >= 60
+    ) {
+      myNav.classList.add("scroll");
+      mybutton.style.display = "block";
     } else {
-        myNav.classList.remove("scroll");
+      myNav.classList.remove("scroll");
+      mybutton.style.display = "none";
     }
+  }
 };
+
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
+
+AOS.init();
