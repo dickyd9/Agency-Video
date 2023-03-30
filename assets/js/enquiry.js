aafc4bb9627934cbfe1671fdfafaa0306a78e3ad
dickyd9 <dickyd6@gmail.com>
2023-03-30 08:17:50 +0700

    send email

---
+++ assets/js/enquiry.js
@@ -0,0 +1,26 @@
+const nama = document.getElementById("nama");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const message = document.getElementById("message");
+const submit = document.getElementById("submit");
+
+submit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let ebody = `
+    <b>Name: </b>${nama.value}
+    <br>
+    <b>Email: </b>${email.value}
+    <br>
+    <b>Phone: </b>${phone.value}
+    <br>
+    <b> Message: </b>${message.value}
+    `;
+
+  Email.send({
+    SecureToken: "4bff15fd-f160-4086-90b6-dfc1e7d19f68",
+    To: "connect@limaempat.com",
+    From: "connect@limaempat.com",
+    Subject: "Enquiry Client-" + email.value,
+    Body: ebody,
+  }).then((message) => alert(message));
+});
